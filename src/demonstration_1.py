1f1d1ce332b842c465f95208e9970b27bd096b62
Trevor Buchanan <trevor@trevorbuchanan.com>
2020-11-24 14:16:24 -0600

    finished lecture

--- src/demonstration_1.py
+++ src/demonstration_1.py
@@ -31,18 +31,69 @@ class ListNode:
         self.next = None
 
 class MyHashTable:
-    def __init__(self):
+    def __init__(self, capacity = 100):
         # Your code here
+       self.capacity = capacity
+       self.storage = [None for _ in range(self.capacity)]
 
+    def _hash(self, key):
+        return id(key) % self.capacity
 
     def put(self, key, value):
         # Your code here
-
+        i = self._hash(key)
+        if self.storage[i] == None:
+            self.storage[i] = ListNode(key, value)
+        else:
+            print(f"collision at {i}")
+            node = self.storage[i]
+            while node.next:
+                if node.next.key == key:
+                    node.next.value = value
+                    return
+                node = node.next
+            node.next = ListNode(key, value)
+            
 
     def get(self, key):
         # Your code here
-
+        i = self._hash(key)
+        if not self.storage[i]:
+            return -1
+        elif self.storage[i].key == key:
+            return self.storage[i].value
+        else:
+            node = self.storage[i]
+            while node.next:
+                if node.next.key == key:
+                    return node.next.value
+                node = node.next
+        return -1
 
     def remove(self, key: int) -> None:
         # Your code here
+        i = self._hash(key)
+        if self.storage[i]:
+            prev = None
+            cur = self.storage[i]
+            while cur:
+                if cur.key == key:
+                    if prev:
+                        prev.next = cur.next
+                        return
+                    else:
+                        self.storage[i] = cur.next
+                        return
+                prev = cur
+                cur = cur.next 
 
+
+hash_table = MyHashTable()
+hash_table.put("a", 1)
+hash_table.put("b", 2)
+print(hash_table.get("a"))            
+print(hash_table.get("c"))            
+hash_table.put("b", 1)         
+print(hash_table.get("b"))            
+hash_table.remove("b")         
+print(hash_table.get("b")) 
--- src/demonstration_2.py
+++ src/demonstration_2.py
@@ -55,4 +55,30 @@ def are_words_sorted(words, alpha_order):
     bool
     """
     # Your code here
+    order = {value: index for index, value in enumerate(alpha_order)}
+    for wi in range(1, len(words)):
+        for ci in range(len(words[wi - 1])):
+            if len(words[wi]) < ci:
+                if words[wi] == words[wi -1 ][0:len(words[wi])]:
+                    return False
+                break
+            elif words[wi - 1][ci] > words[wi][ci]:
+                return False
+    return True
 
+def are_words_sorted_instructor(words, alpha_order):
+    altered_order = {letter: index for index, letter in enumrate(alpha_order)}
+    for i in range(1, len(words)):
+        word1 = words[i-1]
+        word2 = words[i]
+        for j in range(min(len(word1), len(word2))):
+            char1 = word1[j]
+            char2 = word2[j]
+            if altered_order[char1] > altered_order[char2]:
+                return False
+        if len(word1) > len(word2):
+            return False
+    return True
+words = ["lambda","lamb"]
+order = "abcdefghijklmnopqrstuvwxyz"
+print(are_words_sorted(words, order))
